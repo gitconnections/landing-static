e14b42675b2dcd7ea8b6e41a467a57c02502b9c5
Oscar Cortez <ocortez@profunded.com>
2026-06-22 13:21:13 -0400

    feat: reactbits/SocialIcon re-export shim (path alias)

---
+++ components/reactbits/SocialIcon.tsx
@@ -0,0 +1,3 @@
+// Re-export shim: SocialIcon lives at @/components/SocialIcon. This alias makes the
+// @/components/reactbits/SocialIcon path resolve too (generators sometimes guess it).
+export { default, SocialIcon } from '../SocialIcon';
